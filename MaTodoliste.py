c30f20d2ff77ec217567a9aafc2d2a7bc1559d5c
Punkyherisson <emmanuel.goitia@gmail.com>
2025-05-02 17:52:33 +0200

    ✨ Refactorisation du script en version 0.2 : fonctions + structure

--- MaTodoliste.py
+++ MaTodoliste.py
@@ -1,82 +1,57 @@
-#Essai de Todo en Python V0.1 ajout de nouvelles taches 
-maDate=input("Entrez une date au format jj/mm/aaaa : ")
-#Espagnol
-tacheDuolingo=input("Avez-vous fait Duolingo : ")
-tacheExosDele=input("Avez-vous fait des exercices dele : ")
-tacheNoticias=input("Avez-vous lu les noticias : ")
-tacheMosalingua=input("Avez-vous fait des mosalingua spanish: ")
-tacheCafeynVocable=input("Avez-vous fait un caféyn vocable : ")
-tacheMondly=input("Avez-vous fait Mondly : ")
-#Japonais
-tacheKana=input("Avez-vous fait Kana : ")
-tacheDrops=input("Avez-vous fait les drops : ")
-tacheMosaJaponais=input("Avez-vous fait Japonais : ")
-tachekatakanapro=input("Avez-vous fait katakanapro : ")
-tacheMinnaNoNihongo=input("Avez-vous fait Minna no Nihongo : ")
-tacheDevoirsJaponais=input("Avez-vous fait des devoirs japonais : ")
-#Python
-tache100days=input("Avez-vous fait 100 jours de code : ")
-tacheCodeAcademy=input("Avez-vous fait Code Academy : ")
-tachePybites=input("Avez-vous fait Pybites : ")
-tacheDatacamp=input("Avez-vous fait Datacamp : ")
-tacheKaggle=input("Avez-vous fait Kaggle : ")
-tacherepli=input("Avez-vous fait Repli : ")
-tacheRealPython=input("Avez-vous fait Real Python : ")
-tacheProjetPython=input("Avez-vous fait un projet Python : ")
-#Sport/Sante
-tacheSport=input("Avez-vous fait du sport : ")
-tacheApprentissage=input("Avez-vous fait de l'apprentissage : ")
-tacheFruits=input("Avez-vous mangé des fruits : ")
-tacheLegumes=input("Avez-vous mangé des légumes : ")
-tacheProteines=input("Avez-vous mangé des protéines : ")
-#Famille
-tacheFamille=input("Avez-vous fait un truc pour la  famille : ")
+def demander_taches(categorie, taches):
+    print(f"\n--- {categorie} ---")
+    reponses = {}
+    for tache in taches:
+        reponses[tache] = input(f"Avez-vous fait {tache} ? ")
+    return reponses
 
+def ecrire_section(fichier, titre, taches):
+    fichier.write(f"\nObjectifs {titre}\n")
+    for tache, reponse in taches.items():
+        fichier.write(f"{tache} : {reponse}\n")
 
-#on affiche les résultats dans le fichier todo
-fichier=open("todo.txt","w")
-#écrire dans le fichier
-fichier.write("Date : "+maDate+"\n\n")
-fichier.write("Version 01 : ")
-#Espagnol
-fichier.write("Objectifs Espagnol\n")            
-fichier.write("Duolingo : "+tacheDuolingo+"\n")
-fichier.write("Exos dele : "+tacheExosDele+"\n")
-fichier.write("Noticias : "+tacheNoticias+"\n")
-fichier.write("Mosalingua spanish: "+tacheMosalingua+"\n")
-fichier.write("Caféyn vocable : "+tacheCafeynVocable+"\n")
-fichier.write("Mondly : "+tacheMondly+"\n")
-            
-#Japonais
-fichier.write("Objectifs Japonais\n")
-fichier.write("Kana : "+tacheKana+"\n")
-fichier.write("Drops : "+tacheDrops+"\n")
-fichier.write("Japonais : "+tacheMosaJaponais+"\n")
-fichier.write("Katakanapro : "+tachekatakanapro+"\n")
-fichier.write("Minna no Nihongo : "+tacheMinnaNoNihongo+"\n")
-fichier.write("Devoirs japonais : "+tacheDevoirsJaponais+"\n")
-#Python
-fichier.write("Objectifs Python\n")
-fichier.write("100 jours de code : "+tache100days+"\n")
-fichier.write("Code Academy : "+tacheCodeAcademy+"\n")
-fichier.write("Pybites : "+tachePybites+"\n")
-fichier.write("Datacamp : "+tacheDatacamp+"\n")
-fichier.write("Kaggle : "+tacheKaggle+"\n")
-fichier.write("Repli : "+tacherepli+"\n")
-fichier.write("Real Python : "+tacheRealPython+"\n")
-fichier.write("Projet Python : "+tacheProjetPython+"\n")
-#Sport/Sante
-fichier.write("Objectifs Sport/Sante\n")
-fichier.write("Sport : "+tacheSport+"\n")
-fichier.write("Apprentissage : "+tacheApprentissage+"\n")
-fichier.write("Fruits : "+tacheFruits+"\n")
-fichier.write("Legumes : "+tacheLegumes+"\n")
-fichier.write("Proteines : "+tacheProteines+"\n")
-              
-#Famille
-fichier.write("Objectifs Famille\n")
-fichier.write("Famille : "+tacheFamille+"\n")
-#fermer le fichier
-fichier.close()
+def main():
+    print("# Essai de Todo en Python V0.1")
+    maDate = input("Entrez une date au format jj/mm/aaaa : ")
 
+    espagnol = [
+        "Duolingo", "des exercices dele", "lu les noticias", 
+        "des mosalingua spanish", "un caféyn vocable", "Mondly"
+    ]
+    japonais = [
+        "Kana", "les drops", "Japonais", 
+        "katakanapro", "Minna no Nihongo", "des devoirs japonais"
+    ]
+    python = [
+        "100 jours de code", "Code Academy", "Pybites", 
+        "Datacamp", "Kaggle", "Repli", "Real Python", "un projet Python"
+    ]
+    sport_sante = [
+        "du sport", "de l'apprentissage", 
+        "des fruits", "des légumes", "des protéines"
+    ]
+    famille = ["un truc pour la famille"]
+
+    # Collecte des données
+    taches_espagnol = demander_taches("Espagnol", espagnol)
+    taches_japonais = demander_taches("Japonais", japonais)
+    taches_python = demander_taches("Python", python)
+    taches_sante = demander_taches("Sport/Santé", sport_sante)
+    taches_famille = demander_taches("Famille", famille)
+
+    # Écriture dans le fichier
+    with open("todo.txt", "w") as fichier:
+        fichier.write(f"Date : {maDate}\n\n")
+        fichier.write("Version 01 :\n")
+
+        ecrire_section(fichier, "Espagnol", taches_espagnol)
+        ecrire_section(fichier, "Japonais", taches_japonais)
+        ecrire_section(fichier, "Python", taches_python)
+        ecrire_section(fichier, "Sport/Sante", taches_sante)
+        ecrire_section(fichier, "Famille", taches_famille)
+
+    print("\n✅ Les tâches ont été enregistrées dans todo.txt.")
+
+if __name__ == "__main__":
+    main()
               
--- git_initV01.md
+++ git_initV01.md
@@ -1,7 +1,7 @@
 echo "# MaTodolist" >> README.md
 git init
 git add *
-git commit -m "Version O.1 - 28 septembre 2024"
+git commit -m "Version O.2 - 02 mai 2025"
 git branch -M main
 git remote add origin https://github.com/Punkyherisson/MaTodolist.git
 git push -u origin main
